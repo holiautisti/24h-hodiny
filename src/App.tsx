982efac9d2395e4b839c43e01685d5e7b8f8ebc3
st:prchal <prchalek@gmail.com>
2024-02-22 20:37:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,27 @@ function App() {
         <svg width="300" height="300" viewBox="0 0 200 200">
           <circle cx="100" cy="100" r="100" stroke-width="0" fill="#3cbcd4" />
           <line
-            className="clock-big"
+            className="clock-hours"
+            x1="100"
+            y1="100"
+            x2="100"
+            y2="25"
+            stroke-width="7"
+            stroke="#ffffff"
+            stroke-linecap="round"
+          />
+          <line
+            className="clock-minutes"
+            x1="100"
+            y1="100"
+            x2="100"
+            y2="20"
+            stroke-width="5"
+            stroke="#808080"
+            stroke-linecap="round"
+          />
+          <line
+            className="clock-seconds"
             x1="100"
             y1="100"
             x2="100"
