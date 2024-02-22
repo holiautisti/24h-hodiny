b586a2c6cc5a0ed6fc82537e09cc0905c5c18a77
steveruu <prchalek@gmail.com>
2024-02-22 21:26:11 +0100

    nerozumim reactu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Clock from "./Clock.tsx";
 
 /* 24-hour analog clock */
 function App() {
-  // get the current time
-  const [time, setTime] = useState(new Date());
-
-  // update the time every second
-  // this is a side effect, so it goes in a useEffect
+  // const [time, setTime] = useState(new Date());
 
   return (
     <div className="App">
